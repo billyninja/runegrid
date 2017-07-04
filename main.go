656649400749137013f12e189cfe94b6bfe92c0f
billyninja <joaoeduardocm@gmail.com.br>
2017-07-03 22:38:55 -0300

    Day 002 - grid+runes

--- main.go
+++ main.go
@@ -7,119 +7,156 @@ import (
     "os"
 )
 
-const (
-    RUNE_A string = "ᚠ"
-    RUNE_B        = "ᚨ"
-    RUNE_C        = "ᚰ"
-    RUNE_D        = "ᚸ"
-)
-
-type RuneResource struct {
-    Txtr  *sdl.Texture
-    Rect  *sdl.Rect
-    Value string
-}
-
 var (
     CL_WHITE = sdl.Color{255, 255, 255, 255}
+    CL_GREEN = sdl.Color{0, 255, 0, 255}
     CL_BLACK = sdl.Color{0, 0, 0, 255}
+    RUNES    = [4]*RuneResource{}
 )
 
-func HandleEvents() bool {
-    var event sdl.Event
-    for event = sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
-        switch event.(type) {
-        case *sdl.QuitEvent:
-            return false
-            break
+const (
+    RUNA_A string = "ᚠ"
+    RUNA_B        = "ᚨ"
+    RUNA_C        = "ᚰ"
+    RUNA_D        = "ᚸ"
+)
+
+type Vector2d struct {
+    X int32
+    Y int32
+}
+
+type RuneResource struct {
+    Font      *ttf.Font
+    FontSize  int32
+    Value     string
+    Texture   *sdl.Texture
+    BoundRect *sdl.Rect
+}
+
+type RuneInstance struct {
+    Resource *RuneResource
+}
+
+type GridSlot struct {
+    Rect    *sdl.Rect
+    Active  bool
+    Holding *RuneInstance
+}
+
+type Grid struct {
+    RootPos Vector2d
+    Size    int32
+    Spacing int32
+    Slots   [4][4]*GridSlot
+}
+
+func SetColor(r *sdl.Renderer, c sdl.Color) {
+    r.SetDrawColor(c.R, c.G, c.B, c.A)
+}
+
+func (g *Grid) Render(renderer *sdl.Renderer, base_color sdl.Color) {
+    SetColor(renderer, base_color)
+    for i := range g.Slots {
+        for j := range g.Slots[i] {
+            slot := g.Slots[i][j]
+            if slot.Active {
+                SetColor(renderer, CL_GREEN)
+            }
+
+            renderer.DrawRect(slot.Rect)
+            rn := RUNES[j]
+            dest := &sdl.Rect{
+                slot.Rect.X + (slot.Rect.W / 2) - (rn.BoundRect.W / 2),
+                slot.Rect.Y + (slot.Rect.H / 2) - (rn.BoundRect.H / 2),
+                rn.BoundRect.W,
+                rn.BoundRect.H,
+            }
+            renderer.Copy(rn.Texture, rn.BoundRect, dest)
+
+            if slot.Active {
+                SetColor(renderer, base_color)
+            }
         }
     }
+}
 
+func (g *Grid) BuildRects() {
+    for i := 0; i < 4; i++ {
+        for j := 0; j < 4; j++ {
+            x := g.RootPos.X + ((g.Size * int32(i)) + (g.Spacing * int32(i)))
+            y := g.RootPos.Y + ((g.Size * int32(j)) + (g.Spacing * int32(j)))
+            rect := &sdl.Rect{x, y, g.Size, g.Size}
+            g.Slots[i][j] = &GridSlot{
+                Rect:   rect,
+                Active: ((i+j)%2 == 0),
+            }
+        }
+    }
+}
+
+func HandleEvents() bool {
+    var ev sdl.Event
+    for ev = sdl.PollEvent(); ev != nil; ev = sdl.PollEvent() {
+        switch ev.(type) {
+        case *sdl.QuitEvent:
+            return false
+        }
+    }
     return true
 }
 
-func GetFullRect(s *sdl.Surface) *sdl.Rect {
-    return &sdl.Rect{0, 0, s.W, s.H}
-}
-
-func BakeText(r *sdl.Renderer, font *ttf.Font, msg string, color sdl.Color) (*sdl.Texture, *sdl.Rect, error) {
-    surf, err := font.RenderUTF8_Blended(msg, color)
-    if err != nil {
-        return nil, nil, err
-    }
-
-    rect := GetFullRect(surf)
-    t, err := r.CreateTextureFromSurface(surf)
-
-    return t, rect, err
-}
-
-func LoadRune(r *sdl.Renderer, font *ttf.Font, runa string, color sdl.Color) *RuneResource {
-    fmt.Printf("Loading rune %q\n", runa)
-    txtr, rect, _ := BakeText(r, font, runa, color)
+func LoadRune(renderer *sdl.Renderer, font *ttf.Font, color sdl.Color, runa string) *RuneResource {
+    surf, _ := font.RenderUTF8_Blended(runa, color)
+    txtr, _ := renderer.CreateTextureFromSurface(surf)
 
     return &RuneResource{
-        Txtr:  txtr,
-        Rect:  rect,
-        Value: runa,
+        Font:      font,
+        Value:     runa,
+        Texture:   txtr,
+        BoundRect: &sdl.Rect{0, 0, surf.W, surf.H},
     }
 }
 
-func LoadAllRunes(r *sdl.Renderer, font *ttf.Font, color sdl.Color, runes ...string) []*RuneResource {
-    allRunes := make([]*RuneResource, len(runes))
-
+func LoadAllRunes(renderer *sdl.Renderer, font *ttf.Font, color sdl.Color, runes ...string) {
     for i, runa := range runes {
-        allRunes[i] = LoadRune(r, font, runa, color)
+        RUNES[i] = LoadRune(renderer, font, color, runa)
     }
-
-    return allRunes
 }
 
 func run() int {
     var (
-        running  bool
-        window   *sdl.Window
-        renderer *sdl.Renderer
-        err      error
+        running bool = true
+        grid    *Grid
     )
 
-    fmt.Printf("Stating RuneGrid - Day 001\n")
-
-    window, renderer, err = sdl.CreateWindowAndRenderer(320, 540, 0)
+    window, renderer, err := sdl.CreateWindowAndRenderer(320, 480, 0)
     if err != nil {
-        fmt.Printf("Error at sdl.CreateWindowAndRenderer: %v", err)
+        fmt.Printf("Error sdl.CreateWindowAndRenderer %v", err)
         return 1
     }
-
     ttf.Init()
+    f32, err := ttf.OpenFont("assets/fonts/NotoSansRunic-Regular.ttf", 24)
 
-    runeFont, err := ttf.OpenFont("assets/fonts/NotoSansRunic-Regular.ttf", 32)
-    if err != nil {
-        fmt.Printf("Error at ttf.OpenFont: %v", err)
-        return 1
+    window.SetTitle("RuneGrid - Day 002")
+    grid = &Grid{
+        RootPos: Vector2d{32, 32},
+        Size:    60,
+        Spacing: 4,
     }
+    grid.BuildRects()
 
-    window.SetTitle("RuneGrid - Day 001")
+    LoadAllRunes(renderer, f32, CL_WHITE, RUNA_A, RUNA_B, RUNA_C, RUNA_D)
+    println(len(RUNES))
 
-    runesR := LoadAllRunes(renderer, runeFont, CL_WHITE, RUNE_A, RUNE_B, RUNE_C, RUNE_D)
-    println(len(runesR))
-
-    running = true
     for running {
         running = HandleEvents()
+        SetColor(renderer, CL_BLACK)
         renderer.Clear()
-
-        for i, rr := range runesR {
-            dr := *rr.Rect
-            dr.Y = int32(i * 32)
-            renderer.Copy(rr.Txtr, rr.Rect, &dr)
-        }
-
+        grid.Render(renderer, CL_WHITE)
         renderer.Present()
-        sdl.Delay(16)
     }
 
-    sdl.Quit()
     return 0
 }
 
